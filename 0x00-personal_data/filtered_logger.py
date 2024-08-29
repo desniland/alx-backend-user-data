4a258f33802ba8a03c264119cec481c9b4505b98
desniland <albertdesmond26@gmail.com>
2024-08-29 12:22:32 +0300

    Regex-ing->Log->formatter->Create logger->Connect to secure database->Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -89,7 +89,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """formats a LogRecord.
+        """formats  LogRecord.
         """
         msg = super(RedactingFormatter, self).format(record)
         txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
