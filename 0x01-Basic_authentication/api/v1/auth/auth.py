ad50ffeeb558cdb6c6b7982794f9e3cc10445d3f
desniland <albertdesmond26@gmail.com>
2024-09-03 11:29:15 +0300

    Authorization class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,7 @@ class Auth:
     """Authentication class.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
+        """Checking if a path requires authentication.
         """
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
