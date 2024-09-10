0df91b5885dc51b96c4048cecf890748859e273e
desniland <albertdesmond26@gmail.com>
2024-09-10 16:04:08 +0300

    Register New User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,7 +32,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Adds a new user to the database.
+        """function Adds a new user to the database.
         """
         try:
             self._db.find_user_by(email=email)
